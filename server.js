2f572b6573cc3544ac8fb522d94ac4801aa1f75c
Prateek Chandra <prateekchandra27@outlook.com>
2021-11-13 14:26:12 +0530

    Formatting

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.listen(process.env.PORT || 5000,process.env.IP, () => console.log("Server is Running...."));
+app.listen(process.env.PORT || 5000, process.env.IP, () => console.log("Server is Running...."));
