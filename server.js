3f1176ae25829dbe62490e6dc21a2ad397b0514b
Prateek Chandra <prateekchandra27@outlook.com>
2021-11-14 15:38:53 +0530

    Coding and Web Page route added

--- server.js
+++ server.js
@@ -9,5 +9,11 @@ app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+app.get('/coding', (req, res) => {
+    res.render('coding.ejs');
+});
+app.get('/web', (req, res) => {
+    res.render('web.ejs');
+});
 
 app.listen(process.env.PORT || 5000, process.env.IP, () => console.log("Server is Running...."));
