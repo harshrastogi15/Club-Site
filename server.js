e38a09ea02566b0159e2ccfcc1e74ddf4f93df40
Prateek Chandra <prateekchandra27@outlook.com>
2021-11-15 17:57:48 +0530

    Code Optimized

--- server.js
+++ server.js
@@ -10,27 +10,17 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 app.get("/:site", (req, res) => {
-  var site=req.params.site;
-  if(site=='coding')
-    res.render("coding.ejs");
-  else if(site=='web')
-    res.render("web.ejs");
-  else if(site=='ai')
-    res.render("ai.ejs");
-  else if(site=='robotics')
-    res.render("robotics.ejs");
-  else if(site=='blockchain')
-    res.render("blockchain.ejs");
-  else if(site=='cyber')
-    res.render("cyber.ejs");
-  else if(site=='cloud')
-    res.render("cloud.ejs");
-  else if(site=='electronics')
-    res.render("electronics.ejs");
-  else if(site=='automation')
-    res.render("automation.ejs");
-  else
-    res.render("index.ejs");
+  var site = req.params.site;
+  if (site == "coding") res.render("coding.ejs");
+  else if (site == "web") res.render("web.ejs");
+  else if (site == "ai") res.render("ai.ejs");
+  else if (site == "robotics") res.render("robotics.ejs");
+  else if (site == "blockchain") res.render("blockchain.ejs");
+  else if (site == "cyber") res.render("cyber.ejs");
+  else if (site == "cloud") res.render("cloud.ejs");
+  else if (site == "electronics") res.render("electronics.ejs");
+  else if (site == "automation") res.render("automation.ejs");
+  else res.render("index.ejs");
 });
 
 app.listen(process.env.PORT || 5000, process.env.IP, () =>
